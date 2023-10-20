15963092bbb2762dca61a731d857b15ed6da7116
Your Name <you@example.com>
2023-10-21 00:02:23 +0200

    added firebase database and auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 void main() {
   runApp(const MyApp());
 }
