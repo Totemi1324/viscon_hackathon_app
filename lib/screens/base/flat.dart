f48dbf982d819a17165d2c1087c872fff1006e0a
Totemi1324 <nemestomi2@gmail.com>
2023-10-21 01:22:22 +0200

    Automate theme generation from seed

--- lib/screens/base/flat.dart
+++ lib/screens/base/flat.dart
@@ -7,30 +7,17 @@ class Flat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: RadialGradient(
-          center: const Alignment(0.7, -0.6),
-          radius: 2,
-          colors: [
-            Theme.of(context).colorScheme.primaryContainer,
-            Theme.of(context).colorScheme.secondaryContainer,
-          ],
-          stops: const [0.0, 1.0],
-        ),
-      ),
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-        body: Center(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 700),
-            child: body,
-          ),
+        elevation: 0,
+      ),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 700),
+          child: body,
         ),
       ),
     );
