1d8a74f687533077b9b56dffc96dbc4b0c76ce10
Totemi1324 <nemestomi2@gmail.com>
2023-10-21 09:47:43 +0200

    Add labels and validation to login fields

--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -16,10 +16,71 @@ class _LogInFormState extends State<LogInForm> {
   String _email = "";
   String _password = "";
 
+  Widget emailTextField(BuildContext buildContext) => TextFormField(
+        style: Theme.of(context).textTheme.bodyMedium,
+        autocorrect: false,
+        enableSuggestions: true,
+        onFieldSubmitted: (_) =>
+            FocusScope.of(context).requestFocus(_passwordFocusNode),
+        onSaved: (newValue) {
+          if (newValue != null) {
+            _email = newValue;
+          }
+        },
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return "Please enter your email address";
+          }
+          if (!value.trim().endsWith("ethz.ch")) {
+            return "The email you provided is not a valid ETH email address.";
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 3,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+        ),
+      );
+
+  Widget passwordTextField(BuildContext buildContext) => TextFormField(
+        style: Theme.of(context).textTheme.bodyMedium,
+        autocorrect: false,
+        obscureText: true,
+        focusNode: _passwordFocusNode,
+        enableSuggestions: false,
+        onFieldSubmitted: (_) {},
+        onSaved: (newValue) {
+          if (newValue != null) {
+            _email = newValue;
+          }
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 3,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+        ),
+      );
+
   void _onSignInPressed(BuildContext buildContext) /*async*/ {
     setState(() {
       _isLoading = true;
     });
+
+    if (!_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
 
     setState(() {
       _isLoading = false;
@@ -32,40 +93,43 @@ class _LogInFormState extends State<LogInForm> {
       constraints: const BoxConstraints(maxWidth: 400),
       child: Form(
         key: _formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: ListView(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 20),
-              child: TextFormField(
-                style: Theme.of(context).textTheme.bodyMedium,
-                autocorrect: false,
-                enableSuggestions: true,
-                onFieldSubmitted: (_) =>
-                    FocusScope.of(context).requestFocus(_passwordFocusNode),
-                onSaved: (newValue) {
-                  if (newValue != null) {
-                    _email = newValue;
-                  }
-                },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Email address (ending with ethz.ch)",
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: emailTextField(context),
+                  ),
+                ],
               ),
             ),
             Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: TextFormField(
-                  style: Theme.of(context).textTheme.bodyMedium,
-                  autocorrect: false,
-                  obscureText: true,
-                  focusNode: _passwordFocusNode,
-                  enableSuggestions: false,
-                  onFieldSubmitted: (_) {},
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      _email = newValue;
-                    }
-                  },
-                )),
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Password",
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: passwordTextField(context),
+                  ),
+                ],
+              ),
+            ),
             const SizedBox(
               height: 50,
             ),
